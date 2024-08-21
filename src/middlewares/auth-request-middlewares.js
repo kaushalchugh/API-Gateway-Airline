afa861e484a3bdd061ca50af6f1311c8480b53d7
kaushalchugh <kaushalchugh22@gmail.com>
2024-08-21 23:33:52 +0530

    Added authorisation check for add role

--- src/middlewares/auth-request-middlewares.js
+++ src/middlewares/auth-request-middlewares.js
@@ -3,6 +3,7 @@ const { StatusCodes } = require('http-status-codes');
 const { ErrorResponse } = require('../utils/common');
 const AppError = require('../utils/errors/app-error');
 const { UserService } = require('../services');
+const { message } = require('../utils/common/error-response');
 
 function validateAuthRequest(req, res, next) {
     if(!req.body.email) {
@@ -37,7 +38,18 @@ async function checkAuth(req, res, next) {
     
 }
 
+async function isAdmin(req, res, next) {
+    const response = await UserService.isAdmin(req.user)
+    if(!response) { 
+        return res
+                .status(StatusCodes.UNAUTHORIZED)
+                .json({message: 'User not authorized for this action'});
+    }
+    next();
+}
+
 module.exports = {
     validateAuthRequest,
-    checkAuth
+    checkAuth,
+    isAdmin
 }
